e31c2df24b497e348049bdd4e4e1a7fc3ecba540
songlairui <cnre@live.cn>
2017-10-19 22:51:21 +0800

    将逻辑代码，从router中转移到util中

--- src/routes/AdbRouter.ts
+++ src/routes/AdbRouter.ts
@@ -1,12 +1,25 @@
 import { Router, Request, Response, NextFunction } from 'express'
-import { listDevices, listPidsByComm, killProcsByComm } from '../util/devutil'
+import {
+  listDevices,
+  listPidsByComm,
+  killProcsByComm,
+  startMinicap
+} from '../util/devutil'
+import {
+  getDevices,
+  deploy,
+  start,
+  stop,
+  checkRunning,
+  forward,
+  closeRotatorMonitor,
+  getRotatorMonitor
+} from '../util/adbutil'
 
-import * as adb from 'adbkit'
 import * as util from 'util'
 import * as path from 'path'
+import * as adb from 'adbkit'
 let client = adb.createClient()
-
-let tracedDevices = null
 
 export class AdbRouter {
   router: Router
@@ -16,175 +29,82 @@ export class AdbRouter {
   }
 
   public async getDevices(req: Request, res: Response, next: NextFunction) {
-    let devices = await listDevices(client)
-    if (devices) {
-      tracedDevices = devices
+    let { err, devices } = await getDevices()
+    if (err) {
+      res.status(404).send({
+        message: err,
+        code: res.status
+      })
+    } else {
       res.status(200).send({
         message: 'devices here',
         code: res.status,
         devices
       })
-    } else {
-      tracedDevices = null
-      res.status(404).send({
-        message: 'no devices',
-        code: res.status
-      })
     }
   }
   public async deploy(req: Request, res: Response, next: NextFunction) {
-    let device = (tracedDevices || (await listDevices(client)))[0]
-    console.info('[device]: ', device)
-    let abi = device.abi
-    let resources = [
-      {
-        src: path.resolve(
-          __dirname,
-          '../../node_modules',
-          util.format(
-            'minicap-prebuilt/prebuilt/%s/bin/minicap%s',
-            abi.primary,
-            abi.pie ? '' : '-nopie'
-          )
-        ),
-        dest: '/data/local/tmp/minicap',
-        mode: 0o0755
-      },
-      {
-        src: path.resolve(
-          __dirname,
-          '../../node_modules',
-          util.format(
-            'minicap-prebuilt/prebuilt/%s/lib/android-%s/minicap.so',
-            abi.primary,
-            device.sdk
-          )
-        ),
-        dest: '/data/local/tmp/minicap.so',
-        mode: 0o0755
-      },
-      {
-        src: path.resolve(
-          __dirname,
-          '../../node_modules',
-          util.format(
-            'minirev/%s/minirev%s',
-            abi.primary,
-            abi.pie ? '' : '-nopie'
-          )
-        ),
-        dest: '/data/local/tmp/minirev',
-        mode: 0o0755
-      },
-      {
-        src: path.resolve(
-          __dirname,
-          '../../node_modules',
-          util.format(
-            'minitouch-prebuilt/prebuilt/%s/bin/minitouch%s',
-            abi.primary,
-            abi.pie ? '' : '-nopie'
-          )
-        ),
-        dest: '/data/local/tmp/minitouch',
-        mode: 0o0755
-      }
-    ]
-    let result = await Promise.all(
-      resources.map(async resource => {
-        return await client
-          .push(device.id, resource.src, resource.dest, resource.mode)
-          .timeout(10000)
-          .then(function(transfer) {
-            console.info('[transfering],', transfer)
-            return new Promise((resolve, reject) => {
-              transfer.on('error', reject)
-              transfer.on('end', () => resolve('ok'))
-            })
-          })
-          .then(data => {
-            console.info('[transfer result]:', data)
-            return data
-          })
-          .catch(e => {
-            return e.toString()
-          })
-      })
-    )
+    let { err, result } = await deploy()
+
     res.status(200).send({
       result
     })
   }
   public async stop(req: Request, res: Response, next: NextFunction) {
-    let device = (tracedDevices || (await listDevices(client)))[0]
-    if (!device)
-      return res.status(400).send({ message: 'no device', status: res.status })
-    let result = await killProcsByComm(
-      client,
-      device.id,
-      '',
-      '/data/local/tmp/minicap',
-      ''
-    )
+    let { err, result } = await stop()
     res.status(200).send({
       result
     })
   }
 
   public async checkRunning(req: Request, res: Response, next: NextFunction) {
-    let device = (tracedDevices || (await listDevices(client)))[0]
-    if (!device)
-      return res.status(400).send({ message: 'no device', status: res.status })
-    let result = await listPidsByComm(
-      client,
-      device.id,
-      '',
-      '/data/local/tmp/minicap'
-    )
+    let { err, result } = await checkRunning()
     res.status(200).send({
       result
     })
   }
   public async start(req: Request, res: Response, next: NextFunction) {
-    let device = (tracedDevices || (await listDevices(client)))[0]
-    if (!device)
-      return res.status(400).send({ message: 'no device', status: res.status })
-    await killProcsByComm(client, device.id, '', '/data/local/tmp/minicap', '')
-    console.info('start to start')
-    let command = util.format(
-      'LD_LIBRARY_PATH=%s exec %s %s',
-      path.dirname('/data/local/tmp/minicap.so'),
-      '/data/local/tmp/minicap',
-      '-P 540x960@540x960/0'
-    )
-    let result = await client.shell(device.id, command).then(out => {
-      return new Promise((resolve, reject) => {
-        let datachunk = ''
-        out.on('data', chunk => {
-          // console.info('[chunk]')
-          datachunk += chunk
-        })
-        out.on('end', () => {
-          resolve({ datachunk })
-        })
-        out.on('error', error => {
-          reject({ error, command: command })
-        })
-        setTimeout(() => {
-          resolve({ message: 'there is no error in 100ms', code: 0 })
-        }, 100)
-      })
-    })
+    let { err, result } = await start({ orientation: '90' })
     res.status(200).send({
       result
     })
   }
-  public async forward(req: Request, res: Response, next: NextFunction) {}
+  public async forward(req: Request, res: Response, next: NextFunction) {
+    let result = await forward()
+    res.status(200).send({
+      result
+    })
+  }
+
+  public async getRotatorMonitor(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    await getRotatorMonitor()
+    res.send({
+      message: '[exec] getRotatorMonitor'
+    })
+  }
+
+  public async closeRotatorMonitor(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    await closeRotatorMonitor()
+    res.send({
+      message: 'exec closeRotatorMonitor '
+    })
+  }
+
   init() {
     this.router.get('/devices', this.getDevices)
     this.router.get('/deploy', this.deploy)
     this.router.get('/stop', this.stop)
     this.router.get('/start', this.start)
+    this.router.get('/startM', this.getRotatorMonitor)
+    this.router.get('/stopM', this.closeRotatorMonitor)
     this.router.get('/checkRunning', this.checkRunning)
     this.router.get('/forward', this.forward)
   }
@@ -192,4 +112,5 @@ export class AdbRouter {
 
 const adbRouter = new AdbRouter()
 adbRouter.init()
+
 export default adbRouter.router
---
+++ src/util/adbutil.ts
@@ -0,0 +1,199 @@
+import { Router, Request, Response, NextFunction } from 'express'
+import {
+  listDevices,
+  listPidsByComm,
+  killProcsByComm,
+  startMinicap
+} from '../util/devutil'
+
+import * as util from 'util'
+import * as path from 'path'
+import * as adb from 'adbkit'
+let client = adb.createClient()
+
+let tracedDevices = null
+const status = {
+  tryingStart: false
+}
+
+export async function getDevices() {
+  let err
+  let devices = await listDevices(client)
+  if (devices) {
+    tracedDevices = devices
+    return { err, devices }
+  } else {
+    tracedDevices = null
+    return { err: 'no devices', devices }
+  }
+}
+export async function deploy() {
+  let device = (tracedDevices || (await listDevices(client)))[0]
+  let abi = device.abi
+  let err = null
+  let resources = [
+    {
+      src: path.resolve(
+        __dirname,
+        '../../node_modules',
+        util.format(
+          'minicap-prebuilt/prebuilt/%s/bin/minicap%s',
+          abi.primary,
+          abi.pie ? '' : '-nopie'
+        )
+      ),
+      dest: '/data/local/tmp/minicap',
+      mode: 0o0755
+    },
+    {
+      src: path.resolve(
+        __dirname,
+        '../../node_modules',
+        util.format(
+          'minicap-prebuilt/prebuilt/%s/lib/android-%s/minicap.so',
+          abi.primary,
+          device.sdk
+        )
+      ),
+      dest: '/data/local/tmp/minicap.so',
+      mode: 0o0755
+    },
+    {
+      src: path.resolve(
+        __dirname,
+        '../../vendor',
+        util.format(
+          'minirev/%s/minirev%s',
+          abi.primary,
+          abi.pie ? '' : '-nopie'
+        )
+      ),
+      dest: '/data/local/tmp/minirev',
+      mode: 0o0755
+    },
+    {
+      src: path.resolve(
+        __dirname,
+        '../../node_modules',
+        util.format(
+          'minitouch-prebuilt/prebuilt/%s/bin/minitouch%s',
+          abi.primary,
+          abi.pie ? '' : '-nopie'
+        )
+      ),
+      dest: '/data/local/tmp/minitouch',
+      mode: 0o0755
+    }
+  ]
+  let result = await Promise.all(
+    resources.map(async resource => {
+      return await client
+        .push(device.id, resource.src, resource.dest, resource.mode)
+        .timeout(10000)
+        .then(function(transfer) {
+          console.info('[transfering],', transfer)
+          return new Promise((resolve, reject) => {
+            transfer.on('error', reject)
+            transfer.on('end', () => resolve('ok'))
+          })
+        })
+        .then(data => {
+          console.info('[transfer result]:', data)
+          return data
+        })
+        .catch(e => {
+          return e.toString()
+        })
+    })
+  )
+  return { err, result }
+}
+export async function stop() {
+  let device = (tracedDevices || (await listDevices(client)))[0]
+  let err = null
+  if (!device)
+    return {
+      err: new Error('no device'),
+      result: null
+    }
+  let result = await killProcsByComm(
+    client,
+    device.id,
+    '',
+    '/data/local/tmp/minicap',
+    ''
+  )
+  status.tryingStart = false
+  return { err, result }
+}
+
+export async function checkRunning() {
+  let err
+  let device = (tracedDevices || (await listDevices(client)))[0]
+  if (!device)
+    return {
+      err: new Error('no device'),
+      result: null
+    }
+  let result = await listPidsByComm(
+    client,
+    device.id,
+    '',
+    '/data/local/tmp/minicap'
+  )
+  return { err, result }
+}
+export async function start({ orientation }) {
+  let err, result
+  if (status.tryingStart) {
+    console.info('trying start')
+    return {
+      message: 'already trying',
+      result,
+      err
+    }
+  }
+  status.tryingStart = true
+
+  result = await startMinicap({ status, tracedDevices, client, orientation })
+  status.tryingStart = false
+  return { err, result }
+}
+export async function forward() {
+  return {
+    message: 'will use openLocal do forward without port'
+  }
+}
+
+export async function closeRotatorMonitor() {
+  let device = (tracedDevices || (await listDevices(client)))[0]
+  return killProcsByComm(client, device.id, '', 'app_process', '')
+}
+
+export async function getRotatorMonitor() {
+  await closeRotatorMonitor()
+  let device = (tracedDevices || (await listDevices(client)))[0]
+  let apk_path = /\/.*\.apk/.exec(
+    (await client
+      .shell(device.id, `pm path jp.co.cyberagent.stf.rotationwatcher`)
+      .then(adb.util.readAll)).toString()
+  )[0]
+  if (!apk_path) return console.error('no apk_path,', apk_path)
+  let command = `export CLASSPATH="${apk_path}";exec app_process /system/bin jp.co.cyberagent.stf.rotationwatcher.RotationWatcher`
+  // let out = await client.shell(device.id, command)
+  // treatOut(out)
+  return client.shell(device.id, command)
+}
+function treatOut(out) {
+  // out.on('readable', () => {
+  //   let data = out.read()
+  //   if (data) {
+  //     console.info('OUTPUT:', data.toString())
+  //   } else {
+  //     console.info(null)
+  //   }
+  // })
+  out.on('close', () => {
+    console.info('[RotatorMonitor ] close')
+  })
+}
